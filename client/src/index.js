eefbd231c512b5c2b95ca7af3958865b6e9d467e
SixTRaps <651077152@qq.com>
2021-11-16 13:45:04 -0800

    Nov16_update_routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,48 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useForm } from "react-hook-form";
 
-const userTarget = document.querySelector("#user");
-const signupTarget = document.querySelector("#signup");
+import Navigation from "./Components/navigation";
 
-function User() {
+function App() {
   return (
     <div>
-      <a href="signin.html" className="btn">
-        Sign in
-      </a>
-      <a href="signup.html" className="btn">
-        Sign up
-      </a>
+      <Navigation />
     </div>
   );
 }
 
-function Signup() {
-  const { register, handleSumbit } = useForm();
-  const onSubmit = (d) => alert(JSON.stringify(d));
-
-  return (
-    <form onSubmit={handleSumbit(onSubmit)}>
-      <label>
-        Username:
-        <input {...register("username")} />
-      </label>
-      <label>
-        Password:
-        <input type="Password" {...register("password")} />
-      </label>
-      <label>
-        Firstname:
-        <input {...register("firstname")} />
-      </label>
-      <label>
-        Lastname:
-        <input {...register("lastname")} />
-      </label>
-    </form>
-  );
-}
-
-ReactDOM.render(<User />, userTarget);
-ReactDOM.render(<Signup />, signupTarget);
+ReactDOM.render(<App />, document.querySelector("#root"));
