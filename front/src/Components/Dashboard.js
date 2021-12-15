3202bc298e8f9c11ad7793905cde9c4077880cb5
SixTRaps <651077152@qq.com>
2021-12-15 11:34:01 -0800

    bug_fixed

--- front/src/Components/Dashboard.js
+++ front/src/Components/Dashboard.js
@@ -35,9 +35,8 @@ export default function Dashboard() {
             setBalance(curBalance);
             setMsg("");
           }
-        } else {
-          setMsg("");
         }
+        setMsg("");
       }
     }
     lookup();
